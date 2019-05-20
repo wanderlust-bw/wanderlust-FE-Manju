2a20d50ca1ea813f021a1ce5853dc4eac242fd1c
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-05-20 09:24:28 -0700

    testing 1st commit for wanderlust

--- wanderlust/src/Component/Signup.js
+++ wanderlust/src/Component/Signup.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import './Signup.css'
 
 const signUp = ()  => {
 
     return (
-        <div>
-         <h1>Sign Up</h1>
-        </div>
+        <div className='sign-up'>
+            <div>
+            <i id='map' class="fas fa-map"></i>
+            <h3 className='title-text'>WanderLust ...Traveling made easy</h3>
+            </div>
+            <div>
+            <button><i class="fab fa-facebook-square"> Signup with Facebook</i></button>
+            </div>
+            <p>-or-</p>
+            
+            <div>
+                <input 
+                className='email'
+                placeholder='Email'>
+                </input>
+            </div>
+            <div>
+                <input 
+                className='password'
+                placeholder='Password'>
+                </input>
+            </div>
+            <div>
+                <button>Get Started</button>
+            </div>
+         
+         
+        </div> /* sign-up end here */
     )
 
 }
