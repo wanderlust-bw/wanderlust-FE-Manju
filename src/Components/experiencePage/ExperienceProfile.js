2c10d64c4773a9c412b3278dff55347dea2e911e
Manju KC <Manju KC>
2019-05-22 15:01:57 -0400

    created the expereience page

--- src/Components/experiencePage/ExperienceProfile.js
+++ src/Components/experiencePage/ExperienceProfile.js
@@ -1,15 +1,38 @@
 import React from "react"
+import "./experence.css"
 
-class ProfilePage extends React.Component {
+class ExperienceProfie extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name:"",
+            description:"",
+            location:""
+        }
+
+    }
+
+    handleChange = e => {
+        let target = e.target;
+        let name = target.name;
+        this.setState({ [name]: e.target.value })
+    }
     
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log("This form was submitted with following data");
+        console.log(this.state)
+    }
+
+
   render(){
       return(
-          <div>
-              <form>
-                  <input />
-                  <input />
-                  <input/>
-                  <input/>
+          <div className="profilecomponent">
+              <form className="description" onSubmit={this.handleSubmit}>
+                   <input type="text" id="name" placeholder="Enter Name" name="name" value={this.state.name} onChange={this.handleChange} />
+                  <input type="text"  id="name" placeholder="Enter Description" name="description"  value={this.state.description} onChange={this.handleChange}/>
+                  <input type="text" id="name" placeholder="Enter Location" name="location" value={this.state.location} onChange={this.handleChange} />
+                  <button type="submit">Add Experience</button>
               </form>
 
           </div>
@@ -18,3 +41,4 @@ class ProfilePage extends React.Component {
 
 }
 
+export default ExperienceProfie
