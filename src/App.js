95285ed29212aab92bf574b47d7127db28505321
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-05-22 16:20:47 -0700

    link testing success

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LogIn from "./Components/SignIn"
 import { Route, Switch } from 'react-router-dom'
 import ProfilePage from "./Components/Home"
 import ExperienceProfie from "./Components/experiencePage/ExperienceProfile"
+import TravelGuide from './Components/TravelGuide'
 
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
        <Route exact path="/SignUp" component={SignUp} />
       <Route exact path="/LogIn" component={LogIn} />
       <Route exact path="/ProfilePage" component={ProfilePage}/>
+      <Route exact path="/TravelGuide" component={TravelGuide}/>
       <ExperienceProfie />
       </Switch>
       </div>
