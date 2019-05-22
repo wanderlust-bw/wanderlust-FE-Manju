e99903192a6ba51cb38ee152eed9b2bfacc74d81
Manju KC <Manju KC>
2019-05-21 22:07:09 -0400

    update some codes and change some style

--- wonderlust/src/Components/Home.js
+++ wonderlust/src/Components/Home.js
@@ -6,11 +6,15 @@ class ProfilePage extends React.Component {
 
     constructor() {
         super();
-        this.state={
-          bio:"",
-          name:"",
-          email:"",
-          location:""
+        this.state = {
+            img: "",
+            bio: "",
+            name: "",
+            email: "",
+            location: "",
+            file: '',
+            imagePreviewUrl: '',
+            saved: false
         }
     }
 
@@ -20,75 +24,123 @@ class ProfilePage extends React.Component {
         this.setState({ [name]: e.target.value })
     }
 
-    handleSubmit = e =>{
+    handleSubmit = e => {
         e.preventDefault();
         console.log("This form was submitted with following data");
-            console.log(this.state)
+        console.log(this.state)
+        this.setState({ saved: true })
     }
 
-   
+    handleImageChange = e => {
+        e.preventDefault();
 
-    render(){
-        return(
+        let reader = new FileReader();
+        let file = e.target.files[0];
+
+        reader.onloadend = () => {
+            this.setState({
+                file: file,
+                imagePreviewUrl: reader.result
+            });
+        }
+
+        reader.readAsDataURL(file)
+    }
+
+    render() {
+        let { imagePreviewUrl } = this.state;
+        let imagePreview = null;
+        if (imagePreviewUrl) {
+            imagePreview = (<img value="this.state.img" clasName="review" src={imagePreviewUrl} />);
+        } else {
+            imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
+        }
+
+        return (
             <div>
-            <div>
-                <nav className="profile">
-                <div className="header">
-                <h1>Wanderlust</h1>
+                <div>
+                    <nav className="profile">
+                        <div className="header">
+                            <h1>Wanderlust</h1>
+                        </div>
+
+                        <div className="secondpart">
+                            <div className="text" >
+                                <a className="search" href="#">Search</a>
+                            </div>
+                            <div>
+                                <h1>Profile</h1>
+                            </div>
+                        </div>
+                    </nav>
+                    <div classname="about">
+                    </div>
+
+                    <div>
+                        <form className="form" onSubmit={this.handleSubmit}>
+                            <div className="placeholder">
+                                <input className="imagehandle fileInput" type="file" onChange={this.handleImageChange} />
+                                <button className="submitButton" type="submit" onClick={this.uploadImage}>Upload Image</button>
+                            </div>
+                            <div className="imgPreview">
+                                {imagePreview}
+                            </div>
+                            <div className="maindiv">
+                                <span className="submit">Bio : </span>
+                                <input className="textitem" type="name" id="name" placeholder="Enter Name" name="bio" value={this.state.bio} onChange={this.handleChange} />
+                            </div>
+                            <div className="maindiv">
+                                <span className="submit" >Name : </span>
+                                <input className="textitem" type="name" id="name" placeholder="Enter Name" name="name" value={this.state.name} onChange={this.handleChange} />
+                            </div>
+                            <div className="maindiv">
+                                <span className="submit">Email : </span>
+                                <input className="textitem" type="email" id="email" placeholder="Enter Email" name="email" value={this.state.email} onChange={this.handleChange} />
+                            </div>
+                            <div className="maindiv">
+                                <span className="submit">Location : </span>
+                                <input className="textitem" type="name" id="name" placeholder="Enter Location" name="location" value={this.state.location} onChange={this.handleChange} />
+                            </div>
+                            <div className="maindiv">
+                                <button className="botton" type="submit">Save</button>
+                            </div>
+
+                            {
+                            this.state.saved && (
+                                <div>
+                                    <div className="Card">
+                                        <div>
+                                       <p className="paragraph"> Congratulation your profile has been created succesfully!! </p>
+                                        </div>
+                                        <div className="">
+                                         {this.state.bio}
+                                         </div>
+                                         <div>
+                                         {this.state.name}
+                                         </div>
+                                         <div>
+                                         {this.state.email}
+                                         </div>
+                                         <div>
+                                         {this.state.location}
+                                         </div>
+                                    </div>
+                                   
+                                </div>
+                            )
+                        }
+                        </form>
+                      
+
+                    </div>
                 </div>
-                <div className="secondpart">
-                <div className="text" >
-                <a  className="search" href="#">Search</a> 
-                 </div>
-                 <div>
-                 <h1>Profile</h1>
-                 </div>
-                 </div>
-                 </nav>
-                 <div classname="about">
-              <div className="message">
-              <img/>
-              <a className="anchor" href="#">Profile</a>
-              <a className="anchor" className="anchor" href="#">Message</a>
-              <a className="anchor" href="#">Experiences</a>
-              <a className="anchor" href="#">Payments</a>
-              </div>
+
             </div>
-         
-            
-            <div>
-                <form  className="form" onSubmit={this.handleSubmit}>
-                    <div className="maindiv">
-                <span className="textitem">Bio : </span>
-                <input  className="textitem" type="name" id="name" placeholder="Enter Name" name="bio" value={this.state.bio} onChange={this.handleChange}/>
-                </div>
-                <div className="maindiv">
-                <span className="textitem" >Name : </span>
-                <input className="textitem"  type="name" id="name" placeholder="Enter Name" name="name" value={this.state.name} onChange={this.handleChange}/>
-                </div>
-                <div className="maindiv">
-                <span className="textitem">Email : </span>
-                <input className="textitem"  type="email" id="email" placeholder="Enter Email" name="email" value={this.state.email} onChange={this.handleChange}/>
-                </div>
-                <div className="maindiv">
-                <span className="textitem">Location : </span>
-                <input className="textitem" type="name" id="name" placeholder="Enter Location" name="location" value={this.state.location} onChange={this.handleChange}/>
-                </div>
-                <div className="maindiv">
-                <button className="botton" type="submit">Save</button>
-                </div>
-                </form>
-         
-            </div>
-            </div>
- 
-        </div>
 
         )
     }
 }
 
 export default ProfilePage
-    
 
-    
+
