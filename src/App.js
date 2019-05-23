d7b0049cf694c8e41120c4cf833f0ab12b6e6618
Manju KC <Manju KC>
2019-05-22 21:05:36 -0400

    Refactored code

--- src/App.js
+++ src/App.js
@@ -7,18 +7,17 @@ import ProfilePage from "./Components/Home"
 import ExperienceProfie from "./Components/experiencePage/ExperienceProfile"
 import TravelGuide from './Components/TravelGuide'
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Switch >
-       <Route exact path="/SignUp" component={SignUp} />
-      <Route exact path="/LogIn" component={LogIn} />
-      <Route exact path="/ProfilePage" component={ProfilePage}/>
-      <Route exact path="/TravelGuide" component={TravelGuide}/>
-      <ExperienceProfie />
-      </Switch>
+        <Switch >
+          <Route exact path="/SignUp" component={SignUp} />
+          <Route exact path="/LogIn" component={LogIn} />
+          <Route exact path="/ProfilePage" component={ProfilePage} />
+          <Route exact path="/TravelGuide" component={TravelGuide} />
+          <ExperienceProfie />
+        </Switch>
       </div>
     );
   }
